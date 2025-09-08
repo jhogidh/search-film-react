e10dce30de38796319917dd376476c4ae08df6d7
Joshua <joshuaoberlin7@gmail.com>
2025-09-08 10:56:29 +0700

    [FEATURE] Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Box, Container, Toolbar } from "@mui/material";
 import Footer from "./components/Footer"; // Typo 'Foioter' diperbaiki
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   // State query tidak lagi dibutuhkan di sini karena sudah ditangani oleh router
@@ -22,6 +23,7 @@ function App() {
       </Container>
 
       <Footer />
+      <Analytics />
     </Box>
   );
 }
